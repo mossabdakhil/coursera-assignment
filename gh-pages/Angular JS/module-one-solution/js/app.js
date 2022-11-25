db1e28bba2e77478a2aa4ffd6ddd00409bdadb83
unknown <mossabdakhil@gmail.com>
2022-11-25 09:10:05 +0200

    module one solution

--- gh-pages/Angular JS/module-one-solution/js/app.js
+++ gh-pages/Angular JS/module-one-solution/js/app.js
@@ -1,10 +1,20 @@
 (function () {
-	'use strict'
-	angular.module('ModuleOneSolution', [])
+	'use strict';
+	angular.module('myAng', [])
 
-	.controller('myController' ['$scope', function ($scope) {
-		
-
-
-	}])
-})():
+	.controller('myctrl', ['$scope', function ($scope) {
+		$scope.message;
+		$scope.list;
+		$scope.chk = function () {
+			var word = $scope.list.split(" ");
+				$scope.message = 'Please enter data first';
+			}
+			if (word[3] == undefined) {
+				$scope.message = 'Enjoy!';
+			}
+			else {
+				$scope.message = 'Too much!';
+			}
+		}
+	}]);
+})();
